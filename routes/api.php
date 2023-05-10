38b5eebe7056030c07bfcb295a79c44fa3341350
hkaluuma <hkaluuma@idi.co.ug>
2023-05-11 00:23:49 +0300

    Second Commit working get and post

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+//new importations below
+use App\Models\Post;
+use App\http\Controllers\PostsApiController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -11,9 +15,20 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
+
+//api link is below
+http://localhost/eatraderapi/public/api/posts
 |
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/posts', [PostsApiController::class, 'index']);
+
+Route::post('/posts',[PostsApiController::class, 'store']);
+
+Route::put('/posts/{post}', [PostsApiController::class, 'update']);
+
+Route::delete('/posts/{post}', [PostsApiController::class, 'destroy']);
